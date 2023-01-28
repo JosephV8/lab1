300a9b5847fbbc9aee0b4e8d42e150fc98477ec5
josephV8 <joseph.torbey@lau.edu>
2023-01-28 22:09:43 +0200

    solved

---
+++ OneDrive/Desktop/lab1/Problems.java
@@ -0,0 +1,7 @@
+public class Problems{
+	public Static void main (String[] args){
+		System.out.println("!!!!!!!!!!!!!!!!!");
+		System.out.println("hello, there are a lot");
+		System.out.println("of problems");
+	}
+}
